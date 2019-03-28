8d90beef04627ebd4a755228dd57f1eead108217
molodin <aleksandr@molodin.com>
2019-03-28 18:50:20 +0300

    Initial commit

---
+++ app/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+  // var mouseX = 0,
+  //   mouseY = 0,
+  //   limitX = 100,
+  //   limitY = 80;
+
+  // $(window).mousemove(function (e) {
+  //   var offset = $('.hero').offset();
+  //   mouseX = Math.min(e.pageX - offset.left, limitX);
+  //   mouseY = Math.min(e.pageY - offset.top, limitY);
+  //   if (mouseX < 0) mouseX = 0;
+  //   if (mouseY < 0) mouseY = 0;
+  // });
+
+  // var follower = $(".cosmonaut");
+  // var xp = 0,
+  //   yp = 100;
+  // var loop = setInterval(function () {
+  //   xp += (mouseX - xp) / 120;
+  //   yp += (mouseY - yp) / 120;
+  //   follower.css({
+  //     left: xp,
+  //     top: yp
+  //   });
+
+  // }, 1);
+
+});
