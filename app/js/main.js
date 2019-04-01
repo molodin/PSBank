638567df89ec433c1a653ecc95478b944a41cfb2
molodin <aleksandr@molodin.com>
2019-04-01 22:27:30 +0300

    Add two page

--- app/js/main.js
+++ app/js/main.js
@@ -1,29 +1,51 @@
 $(document).ready(function () {
 
-  // var mouseX = 0,
-  //   mouseY = 0,
-  //   limitX = 100,
-  //   limitY = 80;
+  $('.btn-start').on('click', function (e) {
+    $('.hero-content-srart').addClass('off');
+    $('.hero-content-two').removeClass('off');
+    $('.hero-content-two').css({
+      animation: 'content-show ease-out 1s'
+    });
+    e.preventDefault();
+  });
 
-  // $(window).mousemove(function (e) {
-  //   var offset = $('.hero').offset();
-  //   mouseX = Math.min(e.pageX - offset.left, limitX);
-  //   mouseY = Math.min(e.pageY - offset.top, limitY);
-  //   if (mouseX < 0) mouseX = 0;
-  //   if (mouseY < 0) mouseY = 0;
-  // });
+  $('#sex').select2({
+    minimumResultsForSearch: -1
+  });
+  $('#who').select2({
+    minimumResultsForSearch: -1,
+    placeholder: 'Кого поздравляем?'
+  });
 
-  // var follower = $(".cosmonaut");
-  // var xp = 0,
-  //   yp = 100;
-  // var loop = setInterval(function () {
-  //   xp += (mouseX - xp) / 120;
-  //   yp += (mouseY - yp) / 120;
-  //   follower.css({
-  //     left: xp,
-  //     top: yp
-  //   });
+  $('#name').on('input', function () {
+    $('.greeting .greeting-name').text($(this).val() + ',');
+  });
 
-  // }, 1);
+  $('.greeting .greeting-sex').text($('#sex option:selected').text());
+  $('#sex').on('change', function () {
+    $('.greeting .greeting-sex').text($('#sex option:selected').text());
+  });
+
+  $('#who').on('change', function () {
+    $('.greeting .greeting-who').text($('#who option:selected').text());
+  });
+
+  $('#who option[value="woman"]').prop('disabled', true);
+  $('#sex').on('change', function () {
+    if ($('#sex').val() == 'woman') {
+      $('#who').addClass('t');
+      $('#who option[value="man"]').prop('disabled', true);
+      $('#who option[value="woman"]').prop('disabled', false);
+    } else {
+      $('#who').removeClass('t');
+      $('#who option[value="woman"]').prop('disabled', true);
+      $('#who option[value="man"]').prop('disabled', false);
+    }
+    $('#who').select2({
+      minimumResultsForSearch: -1,
+      placeholder: 'Кого поздравляем?',
+    });
+    $("#who").val('').trigger('change');
+  });
 
 });
